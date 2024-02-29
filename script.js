dd9d00f17df483e3b019fb1703a5fbd4a7bc5e62
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-29 09:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your code here
+const images = document.querySelectorAll('.image');
+
+images.forEach(image => {
+    image.addEventListener('dragstart', dragStart);
+    image.addEventListener('dragover', dragOver);
+    image.addEventListener('drop', drop);
+});
+
+let draggedId;
+
+function dragStart(event) {
+    draggedId = event.target.id;
+    // event.dataTransfer.setData('text/plain', draggedId);
+}
+
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    const droppedId = event.target.id;
+    const draggedElement = document.getElementById(draggedId);
+    const droppedElement = document.getElementById(droppedId);
+    
+    // Swap the contents of the divs
+    const tempHTML = draggedElement.innerHTML;
+    draggedElement.innerHTML = droppedElement.innerHTML;
+	draggedElement.id = droppedId;
+    droppedElement.innerHTML = tempHTML;
+	droppedElement.id = draggedId;
+}
